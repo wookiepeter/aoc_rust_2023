abbcfb2b54710229b09d81f5989537807069119d
Matthis Hagen <matthis.hagen@o2online.de>
2024-04-01 18:15:45 +0200

    Start implementing day 17

--- day-17/src/bin/part1.rs
+++ day-17/src/bin/part1.rs
@@ -24,6 +24,18 @@ fn process(input: &str) -> String {
     input.to_string()
 }
 
+struct GraphState {
+    position: Point,
+    heat: usize,
+    last_direction: Direction,
+}
+
+impl GraphState {
+    fn evaluate(&self, grid: &Grid<usize>) {
+        manhattan_dist(lhs, rhs)
+    }
+}
+
 // this would be more ammo efficient if an actual tree was used instead of
 // copying all the path's each time, but should work fine for this purpose.
 fn recursive_dfs(
@@ -32,18 +44,24 @@ fn recursive_dfs(
     visited_nodes: &mut HashMap<Point, (usize, usize)>,
     grid: &Grid<usize>,
 ) {
-    // What data do i need to save for a path
+    // What data do i need to save for a path?
+    // I don't actually need the path, i just need the cost to get there!
     //
-    // definitely the past 3 directions (maybe just all directions?)
-    // definitely the total heat so far
+    // Instead of past 3 direction i can just save current direction and the
+    // amount of steps since the direction changed, could maybe even simplify to horizontal, / vertical
+    // the total heat so far
     // current position
-    // Keep a dict with all already visited positions, that has info required to throw away the current path
-    //      -> # of steps used to reach this node + heat generated on that path
 
-    // TODO: add an Option result containing the actual part and then just compare
-    // when going back any valid results and return them up the chain!
+    // cool optimisation / simplification i found was to always add all possible states
+    // (as in walk 1 or 2 or 3 steps in each direction) and then sort that into an
+    // the priority queue using current heat and greedy evaluation to keep the sortation
+
+    // Keep a dict with all already visited positions,
+    //      -> # of steps used to reach this node + heat generated on that path?
 
     // TODO: Try and figure out an iterative solution for DFS because of [this](https://www.algobreath.com/notes/recursion-vs-iteration-in-rust)
+
+    // use a BinaryHeap to do the priority queueing -> just need to flip the order!
 
     if path.len() == 0 {
         // do start
@@ -51,7 +69,7 @@ fn recursive_dfs(
 }
 
 // the smaller the better
-fn greedy_evaluation(position: Point, grid: &Grid<usize>) -> usize {
+fn process_step(position: Point, grid: &Grid<usize>, step: (Direction, usize)) -> Option<()> {
     manhattan_dist(&grid.size, &position) + grid.get(position).unwrap()
 }
 
@@ -61,7 +79,21 @@ mod tests {
 
     #[test]
     fn test_example() {
-        let result = process("");
+        let result = process(
+            "2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533",
+        );
         assert_eq!(result, "4".to_string())
     }
 }
