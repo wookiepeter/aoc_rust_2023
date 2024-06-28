297d40c9d5292a9dccb11f7bb6419cbca1240590
Matthis Hagen <matthis.hagen@o2online.de>
2024-06-28 13:43:15 +0200

    Create files for part 2

--- day-21/src/bin/part2.rs
+++ day-21/src/bin/part2.rs
@@ -6,6 +6,16 @@ fn main() {
 
 fn process(input: &str) -> String {
     input.to_string()
+
+    // Solution: Divide and conquer
+
+    // The map is not symmetric at all so we have to split everything into the 4 directions
+    // For each direction we have to stages -> the filling stage and then the running stage
+    // in the filling stage the occupied positions are moving into the quadrant
+    // once the quadrants are filled there should be a pattern there that repeats
+    // figure out the length of that pattern and then you have the repeating steps
+
+    // So you have to figure out both of these stages for all quadrants and then you can
 }
 
 #[cfg(test)]
